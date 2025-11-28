3c0323ade1cad8875eaaa4ad8eb11fd0fe990dbc
Carlos <carlincodes.carlossilvior@outlook.com>
2025-11-28 03:33:47 -0300

    Componente campanha: Ajustes e estilizacao

--- src/app/components/campanha/campanha.ts
+++ src/app/components/campanha/campanha.ts
@@ -1,25 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Header } from "../header/header";
 
 @Component({
   selector: 'app-campanha',
-  imports: [CommonModule],
+  imports: [CommonModule, Header],
   templateUrl: './campanha.html',
   styleUrl: './campanha.css',
 })
 export class Campanha {
   titulo = 'Torne-se o Noel de alguém';
   autor = 'Nicolau Batista dos Santos';
-  categoria = 'Social';
-  imagem = '';
+  imagem = 'https://media.gazetadopovo.com.br/2015/12/a90c1a8638cf69548f032fe7c6776316-full.jpg';
   urlCampanha = 'apoia.aqui.com/34209/torne-se-o-noel-de-alguem';
-  sobre = `Olá pessoal! 
-Como vocês sabem, este Natal queremos levar alegria para as crianças do orfanato Ágape. 
-Estamos arrecadando doações para presentes, roupas e alimentos, garantindo um Natal inesquecível para elas. Sua contribuição pode ajudar a 
-tornar o natal de uma criança mágico e inesquecível!`;
+  sobre = `Olá pessoal! Como vocês sabem, este Natal queremos levar alegria para as crianças do orfanato Ágape. 
+Estamos arrecadando doações para presentes, roupas e alimentos, garantindo um Natal inesquecível para elas. Sua contribuição pode ajudar a tornar o natal de uma criança mágico e inesquecível!`;
   mensagens = 102;
   doadores = 256;
-  valorArrecadado = 14570;
+  valorArrecadado = 8570;
   meta = 10000;
 
   get percentual() {
@@ -33,4 +31,9 @@ tornar o natal de uma criança mágico e inesquecível!`;
   denunciar() {
     alert('Campanha denunciada.');
   }
+
+  doar() {
+  alert('Obrigado por contribuir com a campanha! Sua doação foi registrada.');
+}
+
 }
