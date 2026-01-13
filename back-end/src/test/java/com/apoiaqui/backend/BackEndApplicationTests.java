c5af82c3b2d06321a420d792c0ff85e3a906f8da
Daniel <alves.daniel4320@gmail.com>
2026-01-13 09:28:35 -0300

    feat: iniciado backend

---
+++ back-end/src/test/java/com/apoiaqui/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apoiaqui.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
